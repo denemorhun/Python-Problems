a503af65161f3e6844db03cf406ec2021cd004cd
Denem Orhun <denemorhun@gmail.com>
2021-02-04 18:09:33 +0300

    enc

---
+++ AlgoExperts/cipher_alphabet_shift_UNICODE.py
@@ -0,0 +1,34 @@
+''' shift a string by k characters given by int 
+
+    Use chr() and ord() builtin functions
+
+    values must be between 97 and 122 for lower case 
+    must wrap around once it passes
+
+    chr(ord('b')+3)
+
+    # we can also use a hashmap to create our own dict
+
+    # we need to use modulus to wrap around
+
+'''
+
+def caesarCipherEncryptor(string, k):
+    encrypted = []
+
+    if k >= 26:
+        k = k % 26
+	
+    for letter in string:
+        x = ord(letter) + k
+        # if we go past z = 122
+        if x > 122:
+            encrypted.append(chr(96 + x % 122))
+        else:
+        	encrypted.append(chr(x))   
+
+    return "".join(encrypted)
+
+
+
+print(caesarCipherEncryptor('xyz', 28))
